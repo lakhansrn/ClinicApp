56c6518baadfb12b52fec8aa76a52b40c025822f
lakhansrn <lakhansrn@gmail.com>
2015-12-01 21:14:30 +0530

    Added the layers
    Created layers BL,DL,Models

---
+++ ClinicApp/ClinicApp/Models/Patient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ClinicApp.Models
+{
+    [Table("Patient")]
+    public class Patient
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int PersonId { get; set; }
+
+        public virtual UserProfile UserInfo { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public string  RelationPersonName { get; set; }
+
+        public string Address { get; set; }
+    }
+}
